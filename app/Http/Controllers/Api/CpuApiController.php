266ea6afabc22fd2df1755b73e64b373edd9cc81
Oopirata <herovva@gmail.com>
2024-07-31 09:51:14 +0700

    Refactor CpuApiController index method to retrieve latest CPU and backup information

--- app/Http/Controllers/Api/CpuApiController.php
+++ app/Http/Controllers/Api/CpuApiController.php
@@ -21,7 +21,58 @@ class CpuApiController extends Controller
 
     public function index()
     {
-        $data = Cpu::all();
+        $mainquery = "WITH LatestCPU AS (
+                        SELECT
+                            name,
+                            cpu_utilization,
+                            cpu_sql_util,
+                            memory_in_use_mb,
+                            total_memory_mb,
+                            sql_memory_mb,
+                            disk_size,
+                            data_size,
+                            used_data_size,
+                            ROW_NUMBER() OVER (PARTITION BY name ORDER BY created_at DESC) AS rn
+                        FROM
+                            cpu
+                    ),
+                    LatestBackupInfo AS (
+                        SELECT
+                            name,
+                            last_db_backup_date,
+                            ROW_NUMBER() OVER (PARTITION BY name ORDER BY created_at DESC) AS rn
+                        FROM
+                            backup_info
+                    )
+
+                    SELECT 
+                        r.id AS id,
+                        r.nama_rs AS name,
+                        r.nama_server AS server,
+                        ISNULL(CONVERT(varchar, b.last_db_backup_date, 113), 'not available') AS last_db_backup_date,
+                        c.cpu_utilization,
+                        c.cpu_sql_util,
+                        ROUND(((c.memory_in_use_mb + 0.0) / (c.total_memory_mb + 0.0) * 100), 2) AS memory_utilization,
+                        ROUND(((c.data_size + 0.0) / (c.disk_size + 0.0) * 100), 2) AS disk_utilization,
+                        c.total_memory_mb,
+                        c.memory_in_use_mb,
+                        c.sql_memory_mb,
+                        c.disk_size,
+                        c.data_size,
+                        c.used_data_size
+                    FROM 
+                        rsname r
+                    LEFT JOIN 
+                        LatestBackupInfo b 
+                    ON 
+                        r.nama_rs = b.name AND b.rn = 1
+                    LEFT JOIN 
+                        LatestCPU c 
+                    ON 
+                        r.nama_rs = c.name AND c.rn = 1";
+
+
+        $data = DB::select($mainquery);
         return response()->json([
             'status' => 'success',
             'data' => $data
